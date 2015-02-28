4a9cec7b7b8d1214a364ce157b20f5fb119402cb
Martin Hanzel <arthanzel@gmail.com>
2015-02-28 18:08:00 -0500

    Can add course content

--- app/controllers/course_contents_controller.rb
+++ app/controllers/course_contents_controller.rb
@@ -1,12 +1,12 @@
-class CourseContentController < ApplicationController
+class CourseContentsController < ApplicationController
     def create
-        @course_content = CourseContent.new(course_params)
+        @course_content = CourseContent.new(course_content_params)
         @course_content.save
         render text: "saved"
     end
 
     private
         def course_content_params
-            params.require(:course).permit(:name, :description)
+            params.require(:course_content).permit(:course_id, :name, :description)
         end
 end
